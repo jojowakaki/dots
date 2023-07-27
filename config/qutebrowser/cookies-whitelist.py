0601d8c52e16fa4752af5ff39f84465f1028e2cf
jojowakaki <73982843+jojowakaki@users.noreply.github.com>
2023-07-27 15:09:04 +0300

    Add files via upload

---
+++ config/qutebrowser/cookies-whitelist.py
@@ -0,0 +1,6 @@
+config.set('content.cookies.accept', 'all','*://*.google.com/*')
+config.set('content.cookies.accept', 'all','*://*.reddit.com/*')
+config.set('content.cookies.accept', 'all','*://web.telegram.org/*')
+config.set('content.cookies.accept', 'all','*://web.whatsapp.com/*')
+config.set('content.cookies.accept', 'all','*://*.youtube.com/*')
+#add here cookies whitelist
